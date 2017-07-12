ac56fd8e711bda6a9406debb50eac9dffda684c5
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-07-12 15:25:57 +0200

    commented out the remaining tests relying on the old way to express examples via structur()

--- pkg/inst/tests/runit.NlModel.R
+++
@@ -1,6 +0,0 @@
-#
-# vim:set ff=unix expandtab ts=2 sw=2:
-require(RUnit)
-test.GeneralNlModel=function(){
-   attr(GeneralNlModel,"ex")()
-}
--- pkg/inst/tests/runit.nonlinear.R
+++ pkg/inst/tests/runit.nonlinear.R
@@ -1,14 +1,14 @@
+##
+## vim:set ff=unix expandtab ts=2 sw=2:
+#require(RUnit)
+#test.GeneralNlModel=function(){
+#  attr(GeneralNlModel,"ex")()
+#}
 #
-# vim:set ff=unix expandtab ts=2 sw=2:
-require(RUnit)
-test.GeneralNlModel=function(){
-  attr(GeneralNlModel,"ex")()
-}
-
-test.TwopMMmodel=function(){
-attr(TwopMMmodel,"ex")()
-}
-
-test.bacwaveModel=function(){
-attr(bacwaveModel,"ex")()
-}
+#test.TwopMMmodel=function(){
+#attr(TwopMMmodel,"ex")()
+#}
+#
+#test.bacwaveModel=function(){
+#attr(bacwaveModel,"ex")()
+#}
