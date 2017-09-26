790ce791af5575a69737e073d526c20833a8c8c4
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-09-26 19:42:28 +0200

    added a build folder that contains a built packag, that avoids the autocomplition issues at least on my machine.

--- scripts/buildDocs.R
+++ scripts/buildDocs.R
@@ -5,8 +5,8 @@ require(linkeddocs)
 pkgDir='~/SoilR-exp/pkg'
 package.skeleton.dx_3(pkgDir)
 install(pkgDir,args=c('--html'))
-#system('xdg-open ~/R/x86_64-pc-linux-gnu-library/3.3/SoilR//html/00Index.html')
 #check(pkgDir,document=FALSE,build_args = '--no-build-vignettes')
 #system(sprintf('firefox %s/SoilR/html/SoilR_package.html &',.libPaths()[1]))
+#system(sprintf('firefox %s/SoilR/html/00Index.html &',.libPaths()[1]))
 
 
