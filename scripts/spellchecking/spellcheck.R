fba60e82e361bf646a0d03c08005c0325ce36946
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-11-09 23:59:15 +0100

    spellchecking along

--- scripts/spellchecking/spellcheck.R
+++ scripts/spellchecking/spellcheck.R
@@ -5,45 +5,27 @@ pkgName <- 'SoilR'
 initial.options <- commandArgs(trailingOnly = FALSE)
 file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
-script.basename <- dirname(script.name)
 ss<- dirname(script.name)
+ss<- '.'
 ignoreListPath<-  file.path(ss,"manually_maintained_list_of_ignored_words.txt") 
 manDictPath <-  file.path(ss,"acceptedWords.rds") 
 autoDictPath <-  file.path(ss,"SoilR_object_names.rds") 
 saveRDS(readLines(ignoreListPath),manDictPath)
 
-sr=file.path(script.basename,"..","..","pkg")
+sr=file.path(ss,"..","..","pkg")
 # install pkg to be able to find the names of classes and functions
 if (!is.element('devtools',installed.packages())){
 	install.packages('devtools',repos='https://cran.uni-muenster.de')
 }
 require(devtools)
-install(sr)
+#install(sr)
 require(pkgName,character.only=TRUE)
 fqPkgName <- sprintf('package:%s',pkgName)
 print(fqPkgName)
 objectNames<- ls(as.environment(fqPkgName))
-print(objectNames)
-filter <- function(ifile,encoding){
-
-  #do something with the object names here
-}
+#objectNames <- c('TimeMap')
+saveRDS(objectNames,autoDictPath)
 
 #funcList=c(aspell_package_R_files,aspell_package_Rd_files,aspell_package_vignettes)
-res <- aspell(
-  dir=sr,
-  control=list(
-    "--master=en_US"
-    # if we add additional dictionaries less errors are reported
-    # since more words are "known".
-    ,
-    "--add-extra-dicts=en_GB"
-  )
-  ,
-  drop=c('\\author','\\name','\\alias')
-  ,
-  dictionaries=c(manDictPath)
-  ,
-  filter=filter
-)
+res <- aspell_package_Rd_files( dir=sr,control=list( "--master=en_US" , "--add-extra-dicts=en_GB") , drop=c('\\author','\\name','\\alias') , dictionaries=autoDictPath)
 print(res)
