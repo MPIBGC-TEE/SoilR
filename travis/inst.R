854d8d440e4f3ab0d0e51bc4f8973a02c21913ff
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-04-12 15:24:28 +0200

    buildtest

--- travis/inst.R
+++ travis/inst.R
@@ -8,4 +8,4 @@ require(devtools)
 lapply(fromGitHub,function(name){devtools::install_github(sprintf("mamueller/%s/pkg",name))})
 
 # install the package itself from source  
-devtools::install(file.path('..','pkg'))
+devtools::install(file.path('pkg'))
