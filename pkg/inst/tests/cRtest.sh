f3f8b361202606678bbe7b8a2ece42622834f62c
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-06-20 09:12:12 +0200

    further reorganized the tests. Most tests in pkg/inst/tests/src now use loadpkg::load_all to load the complete SoilR package without the namespace restrictions. So also unexported functions can be tested

--- pkg/inst/tests/cRtest.sh
+++
@@ -1,1 +0,0 @@
-continiousRunner.sh Rtest.R /tmp/tmp2 "$sR/*.R ./runit*.R Rtest.R ./protected/runit*.R"
--- pkg/inst/tests/csall.sh
+++
@@ -1,1 +0,0 @@
-continiousRunner.sh sall.R /tmp/ "../../R/*.R  ./runit*.R ./protected/runit*.R ./sall.R"
