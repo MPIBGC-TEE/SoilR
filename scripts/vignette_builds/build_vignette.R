febe30087f47a81ff9c31eed7a3215f688b5db5c
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-06-24 20:31:25 +0200

    More corrections to the vignette

---
+++ scripts/vignette_builds/build_vignette.R
@@ -0,0 +1,20 @@
+#!/usr/bin/Rscript
+require('pkgload')
+pkgDir=file.path('..','..','pkg')
+trunks=c('ParameterEstimation-knitr')
+
+pkgload::load_all(pkgDir)
+require('knitr')
+for (t in trunks){
+  texfileName=file.path(paste(t,'tex',sep='.'))
+  knitr::knit(
+    file.path(
+      pkgDir
+      ,'vignettes'
+      ,paste(t,'Rnw',sep=".")
+    )
+    ,output=texfileName
+  )
+  expr=tools::texi2pdf(texfileName)
+  expr=tools::texi2pdf(texfileName)
+}
