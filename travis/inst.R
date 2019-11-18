2a5eb7ad0bdce3fb945408d95890c4bcc3f8ef4e
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-11-18 20:59:42 +0100

    removed the (now unnecessary) installation of R6Unit,debugHelpers

--- travis/inst.R
+++ travis/inst.R
@@ -3,9 +3,9 @@
 fromCran <- c( 'devtools','testthat', 'argparse', 'stringr','RUnit')
 lapply(fromCran,install.packages,repos='https://cran.uni-muenster.de')
 
-fromGitHub <- c( 'R6Unit','debugHelpers')
-require(devtools)
-lapply(fromGitHub,function(name){devtools::install_github(sprintf("mamueller/%s/pkg",name))})
+#fromGitHub <- c( 'R6Unit','debugHelpers')
+#require(devtools)
+#lapply(fromGitHub,function(name){devtools::install_github(sprintf("mamueller/%s/pkg",name))})
 
 # install the package itself from source  
 devtools::install(file.path('pkg'))
