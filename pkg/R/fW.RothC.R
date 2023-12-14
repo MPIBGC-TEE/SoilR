819904cd7f1536874a04a126fcd47eb21da24ac6
csierra <csierra@bgc-jena.mpg.de>
2023-12-14 08:58:50 +0100

    Fixed bug in RothC soil moisture modifier that affected time series with a first element with negative water balance

--- pkg/R/fW.RothC.R
+++ pkg/R/fW.RothC.R
@@ -18,9 +18,15 @@
 #' @references Coleman, K., and D. S. Jenkinson (1999), RothC-26.3 A model for
 #' the turnover of carbon in soil: model description and windows user guide
 #' (modified 2008), 47 pp, IACR Rothamsted, Harpenden.
-fW.RothC<- structure(
-    function 
-    (P,           
+#' @examples
+#' P=c(74,59,62,51,52,57,34,55,58,56,75,71)
+#' E=c(8,10,27,49,83,99,103,91,69,34,16,8)
+#' Rothamsted=fW.RothC(P,E)
+#' data.frame(Month=month.name,Rainfall=P,
+#' Evaporation=E, pE= (E*0.75), SMD=(P-0.75*E),Rothamsted)
+#' 
+fW.RothC<-function(
+     P,           
      E,           
      S.Thick=23,  
      pClay=23.4,  
@@ -33,7 +39,7 @@ fW.RothC<- structure(
      M=P-E*pE
      Acc.TSMD=NULL
        for(i in 2:length(M)){
-          Acc.TSMD[1]=ifelse(M[1] > 0, 0, M[1])
+          Acc.TSMD[1]=ifelse(M[1] > 0, 0, Max.TSMD)
           if(Acc.TSMD[i-1]+M[i] < 0){
              Acc.TSMD[i]=Acc.TSMD[i-1]+M[i]
           }
@@ -42,14 +48,9 @@ fW.RothC<- structure(
             Acc.TSMD[i]=Max.TSMD
          }
        }
-     b=ifelse(Acc.TSMD > 0.444*Max.TSMD,1,(0.2+0.8*((Max.TSMD-Acc.TSMD)/(Max.TSMD-0.444*Max.TSMD))))
+     b=ifelse(test=Acc.TSMD > 0.444*Max.TSMD, # test uses < instead of > because values are negative
+              yes=1,
+              no=(0.2+0.8*((Max.TSMD-Acc.TSMD)/(Max.TSMD-0.444*Max.TSMD)))
+              )
      return(data.frame(Acc.TSMD,b))
-     }
-     ,
-    ex=function(){
-       P=c(74,59,62,51,52,57,34,55,58,56,75,71) 
-       E=c(8,10,27,49,83,99,103,91,69,34,16,8)  
-       Rothamsted=fW.RothC(P,E)
-       data.frame(month.name,P,E,0.75*E,P-0.75*E,Rothamsted)  
-    }        
-)
+}
