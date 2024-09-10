33300db77c20d3572b32006f9df23df4d1a79f01
csierra <csierra@bgc-jena.mpg.de>
2024-09-10 10:12:34 +0200

    New function to compute radiocarbon inventories of CO2. Updated version to 1.2.110

--- scripts/inst_pkg_dependencies.R
+++ scripts/inst_pkg_dependencies.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
-isradDesc<-desc::desc("../pkg/DESCRIPTION")
-dependencies<-isradDesc$get_deps()
+Desc<-desc::desc("../pkg/DESCRIPTION")
+dependencies<-Desc$get_deps()
 imports<-dependencies[dependencies$type=="Imports",2]
 #print(imports)
 install.packages(pkgs=imports, repos="https://cloud.r-project.org")
